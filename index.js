edf20b4d4a8f38eff1509af30618f36528c3f1af
kezaqueen <rutagengwakezaqueen@gmail.com>
2025-06-05 10:13:19 +0300

    Added DOM activity

--- index.js
+++ index.js
@@ -18,18 +18,20 @@ header3.forEach(header=>{
 
 //Question4
 
-const addFruit = document.querySelector('#fruList');
+const addFruit = document.getElementById('fruList');
 if(addFruit){
     const fruitAdded = document.createElement('li');
     fruitAdded.textContent = 'Pineapple';
+    addFruit.style.display="flex";
+    addFruit.style.justifyContent="space-between";
     addFruit.appendChild(fruitAdded);
 
 };
 const fruitImages = {
-    "Mangoes":"image/Does eating mango increase uric acid levels_.jpeg",
-    "Banana":  "image/The Best Banana Pudding Recipe You'll Ever Find - Southern Love.jpeg",
-    "Watermelon":"image/How to Cut a Watermelon (the Easy Way!).jpeg",
-    "Pineapple" :"image/Pineapple Fruit White Transparent, Pineapple Fruit Pineapple, Pineapple Clipart, Pineapple, Fruit PNG Image For Free Download.jpeg"
+    "Mangoes":"image/real mangoes.jpg",
+    "Bananas":  "image/real bananas.jpeg",
+    "Water Melons":"image/How to Cut a Watermelon (the Easy Way!).jpeg",
+    "Pineapple" :"image/realpineapple.jpg"
 }
 document.querySelectorAll("#fruList li").forEach(li => {
     const fruitName = li.firstChild.textContent.trim();
@@ -37,7 +39,7 @@ document.querySelectorAll("#fruList li").forEach(li => {
       const img = document.createElement("img");
       img.src = fruitImages[fruitName];
       img.alt = fruitName;
-      img.className = "fruit-image";
+      img.className = "fruit-images";
       li.appendChild(img);
     }
   });
@@ -49,6 +51,32 @@ const addVegetables = document.querySelector('#vegList');
 if(addVegetables){
     const vegetableAdded = document.createElement('li');
     vegetableAdded.textContent = 'Eggplant'
+    addVegetables.style.display="flex";
+    addVegetables.style.justifyContent="space-between";
     addVegetables.appendChild(vegetableAdded)
 
 };
+const veggieImages = {
+    "Onions":"image/realonions.jpeg",
+    "Tomatoes":  "image/realtomatoes.jpg",
+    "Kales":"image/realkales (copy).jpg",
+    "Eggplant" :"image/aubergine 👍.jpeg"
+}
+document.querySelectorAll("#vegList li").forEach(li => {
+    const veggieName = li.firstChild.textContent.trim();
+    if (veggieImages[veggieName]) {
+      const img = document.createElement("img");
+      img.src = veggieImages[veggieName];
+      img.alt = veggieName;
+      img.className = "veggie-images";
+      li.appendChild(img);
+    }
+  });
+  const clickButton = document.getElementById("click-button");
+
+clickButton.addEventListener("click",()=>{
+    clickButton.style.backgroundColor = "orange";
+
+});
+
+
